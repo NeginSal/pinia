5b9318ba25acb149e96801859c7f52eb74f7956d
NeginSal <neginsaljooghi@gmail.com>
2023-05-22 11:35:02 +0330

    Edited app.vue and added styles

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <main>
+    <!--Heading-->
+    <header>
+      <img src="./assets/pinia-logo.svg" alt="pinia logo" />
+      <h1>Pinia Task</h1>
+    </header>
+  </main>
+</template>
+
+<script>
+export default {};
+</script>
