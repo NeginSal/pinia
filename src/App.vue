ac872b3d69d194689777adf8eb590cbd70c90813
NeginSal <neginsaljooghi@gmail.com>
2023-05-28 10:01:41 +0330

    added getters

--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,23 @@
       <img src="./assets/pinia-logo.svg" alt="pinia logo" />
       <h1>Pinia Task</h1>
     </header>
+
+    <!--filter-->
+    <nav class="filter">
+      <button @click="filter = 'all'">all</button>
+      <button @click="filter = 'favs'">favs</button>
+    </nav>
+
     <!--Task List-->
-    <div class="task-list">
+    <div class="task-list" v-if="filter === 'all'">
+      <p>all tasks</p>
       <div v-for="task in TaskStore.tasks" :key="task.id">
+        <TaskDetails :task="task" />
+      </div>
+    </div>
+    <div class="task-list" v-if="filter === 'favs'">
+      <p>fav tasks</p>
+      <div v-for="task in TaskStore.favs" :key="task.id">
         <TaskDetails :task="task" />
       </div>
     </div>
@@ -17,13 +31,16 @@
 <script>
 import { useTaskStore } from "./stores/TaskStore";
 import TaskDetails from "./components/TaskDetails.vue";
+import { ref } from "vue";
 
 export default {
   components: { TaskDetails },
 
   setup() {
     const TaskStore = useTaskStore();
-    return { TaskStore };
+    const filter = ref("all");
+
+    return { TaskStore, filter };
   },
 };
 </script>
