55f53628fb190f5e0036e3c892966033c7577676
NeginSal <neginsaljooghi@gmail.com>
2023-05-31 12:00:20 +0330

    added deletetask action

--- src/stores/TaskStore.js
+++ src/stores/TaskStore.js
@@ -23,6 +23,11 @@ export const useTaskStore = defineStore('taskStore', {
   actions: {
     addTask(task) {
       this.tasks.push(task)
+    },
+    deleteTask(id) {
+      this.tasks = this.tasks.filter(task => {
+        return task.id !== id
+      })
     }
   }
 })
